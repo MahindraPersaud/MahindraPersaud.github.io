c5e6b6c04d6462b0900ceca1cf77820e819a9d0b
Mahindra Persaud <mahindrapersaud1@gmail.com>
2017-03-21 14:32:10 -0400

    Uploaded midterm

--- js/main.js
+++ js/main.js
@@ -2,148 +2,148 @@
 
 (function($) {
 
-	"use strict";
+    "use strict";
 
-	/* Preloader */ 
-   $(window).load(function() {
+    /* Preloader */
+    $(window).load(function() {
 
-      // will first fade out the loading animation 
-    	$("#loader").fadeOut("slow", function(){
+        // will first fade out the loading animation 
+        $("#loader").fadeOut("slow", function() {
 
-        // will fade out the whole DIV that covers the website.
-        $("#preloader").delay(300).fadeOut("slow");
+            // will fade out the whole DIV that covers the website.
+            $("#preloader").delay(300).fadeOut("slow");
 
-      });       
+        });
 
-  	})
+    })
 
-  	/* FitText Settings */
-  	setTimeout(function() {
+    /* FitText Settings */
+    setTimeout(function() {
 
-   	$('#intro h1').fitText(1, { minFontSize: '42px', maxFontSize: '84px' });
+        $('#intro h1').fitText(1, { minFontSize: '42px', maxFontSize: '84px' });
 
-  	}, 100);
+    }, 100);
 
-	/* Stat Counter */
-   var statSection = $("#stats"),
-       stats = $(".stat-count");
+    /* Stat Counter */
+    var statSection = $("#stats"),
+        stats = $(".stat-count");
 
-   statSection.waypoint({
+    statSection.waypoint({
 
-   	handler: function(direction) {
+        handler: function(direction) {
 
-      	if (direction === "down") {       		
+            if (direction === "down") {
 
-			   stats.each(function () {
-				   var $this = $(this);
+                stats.each(function() {
+                    var $this = $(this);
 
-				   $({ Counter: 0 }).animate({ Counter: $this.text() }, {
-				   	duration: 4000,
-				   	easing: 'swing',
-				   	step: function (curValue) {
-				      	$this.text(Math.ceil(curValue));
-				    	}
-				  	});
-				});
+                    $({ Counter: 0 }).animate({ Counter: $this.text() }, {
+                        duration: 4000,
+                        easing: 'swing',
+                        step: function(curValue) {
+                            $this.text(Math.ceil(curValue));
+                        }
+                    });
+                });
 
-       	} 
+            }
 
-       	// trigger once only
-       	this.destroy();      	
+            // trigger once only
+            this.destroy();
 
-		},
-			
-		offset: "90%"
-	
-	});	
+        },
 
-  	/* Navigation Menu */  
-   var toggleButton = $('.menu-toggle'),
-       nav = $('.main-navigation');
+        offset: "90%"
 
-   // toggle button
-   toggleButton.on('click', function(e) {
+    });
 
-		e.preventDefault();
-		toggleButton.toggleClass('is-clicked');
-		nav.slideToggle();
+    /* Navigation Menu */
+    var toggleButton = $('.menu-toggle'),
+        nav = $('.main-navigation');
 
-	});
+    // toggle button
+    toggleButton.on('click', function(e) {
 
-   // nav items
-  	nav.find('li a').on("click", function() {   
+        e.preventDefault();
+        toggleButton.toggleClass('is-clicked');
+        nav.slideToggle();
 
-   	// update the toggle button 		
-   	toggleButton.toggleClass('is-clicked'); 
-   	// fadeout the navigation panel
-   	nav.fadeOut();   		
-   	     
-  	});
+    });
+
+    // nav items
+    nav.find('li a').on("click", function() {
+
+        // update the toggle button 		
+        toggleButton.toggleClass('is-clicked');
+        // fadeout the navigation panel
+        nav.fadeOut();
+
+    });
 
 
-  	/* Highlight the current section in the navigation bar */
-	var sections = $("section"),
-	navigation_links = $("#main-nav-wrap li a");	
+    /* Highlight the current section in the navigation bar */
+    var sections = $("section"),
+        navigation_links = $("#main-nav-wrap li a");
 
-	sections.waypoint( {
+    sections.waypoint({
 
-       handler: function(direction) {
+        handler: function(direction) {
 
-		   var active_section;
+            var active_section;
 
-			active_section = $('section#' + this.element.id);
+            active_section = $('section#' + this.element.id);
 
-			if (direction === "up") active_section = active_section.prev();
+            if (direction === "up") active_section = active_section.prev();
 
-			var active_link = $('#main-nav-wrap a[href="#' + active_section.attr("id") + '"]');			
+            var active_link = $('#main-nav-wrap a[href="#' + active_section.attr("id") + '"]');
 
-         navigation_links.parent().removeClass("current");
-			active_link.parent().addClass("current");
+            navigation_links.parent().removeClass("current");
+            active_link.parent().addClass("current");
 
-		}, 
+        },
 
-		offset: '25%'
-	});
+        offset: '25%'
+    });
 
-  	/* Smooth Scrolling */
-  	$('.smoothscroll').on('click', function (e) {
-	 	
-	 	e.preventDefault();
+    /* Smooth Scrolling */
+    $('.smoothscroll').on('click', function(e) {
 
-   	var target = this.hash,
-    	$target = $(target);
+        e.preventDefault();
 
-    	$('html, body').stop().animate({
-       	'scrollTop': $target.offset().top
-      }, 800, 'swing', function () {
-      	window.location.hash = target;
-      });
+        var target = this.hash,
+            $target = $(target);
 
-  	});  
-  
+        $('html, body').stop().animate({
+            'scrollTop': $target.offset().top
+        }, 800, 'swing', function() {
+            window.location.hash = target;
+        });
 
-	/*  Placeholder Plugin Settings */ 
-	$('input, textarea, select').placeholder()  
+    });
 
-  	/* Back to top */ 
-	var pxShow = 300; // height on which the button will show
-	var fadeInTime = 400; // how slow/fast you want the button to show
-	var fadeOutTime = 400; // how slow/fast you want the button to hide
-	var scrollSpeed = 300; // how slow/fast you want the button to scroll to top. can be a value, 'slow', 'normal' or 'fast'
 
-   // Show or hide the sticky footer button
-	jQuery(window).scroll(function() {
+    /*  Placeholder Plugin Settings */
+    $('input, textarea, select').placeholder()
 
-		if (!( $("#header-search").hasClass('is-visible'))) {
+    /* Back to top */
+    var pxShow = 300; // height on which the button will show
+    var fadeInTime = 400; // how slow/fast you want the button to show
+    var fadeOutTime = 400; // how slow/fast you want the button to hide
+    var scrollSpeed = 300; // how slow/fast you want the button to scroll to top. can be a value, 'slow', 'normal' or 'fast'
 
-			if (jQuery(window).scrollTop() >= pxShow) {
-				jQuery("#go-top").fadeIn(fadeInTime);
-			} else {
-				jQuery("#go-top").fadeOut(fadeOutTime);
-			}
+    // Show or hide the sticky footer button
+    jQuery(window).scroll(function() {
 
-		}		
+        if (!($("#header-search").hasClass('is-visible'))) {
 
-	});		
+            if (jQuery(window).scrollTop() >= pxShow) {
+                jQuery("#go-top").fadeIn(fadeInTime);
+            } else {
+                jQuery("#go-top").fadeOut(fadeOutTime);
+            }
+
+        }
+
+    });
 
 })(jQuery);
